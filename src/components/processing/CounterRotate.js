ed7877096015468aaf764db5a71465753d85599d
Ian Andrewson <ianandrewson@gmail.com>
2020-03-13 15:19:42 -0700

    CounterRotate setup function definition implemented. Initial setup renders

--- src/components/processing/CounterRotate.js
+++ src/components/processing/CounterRotate.js
@@ -2,9 +2,21 @@ import React from 'react';
 import Sketch from 'react-p5';
 
 export default function CounterRotate() {
+  const radius1 = 50;
+  const radius2 = 50;
+
+  class Circle {
+    constructor(radius) {
+      this.radius = radius;
+    }
+  }
 
   const setup = p5 => {
-    p5.createCanvas(800, 800);
+    p5.createCanvas(600, 600);
+    p5.background(255, 165, 0);
+    p5.ellipseMode(p5.CORNER);
+    p5.frameRate(30);
+    p5.noStroke();
   };
 
   const draw = p5 => {
