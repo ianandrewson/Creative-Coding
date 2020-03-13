c70bb6cecaf5ae5ebbe42ed1734af1cdca694898
Ian Andrewson <ianandrewson@gmail.com>
2020-03-13 11:23:47 -0700

    implemented move method for circle class

--- src/components/processing/Polaris.js
+++ src/components/processing/Polaris.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Sketch from 'react-p5';
 
 export default function Polaris() {
-
   //refactor these into state for controllers
   const numCircles = 100;
   const pullToTarget = 1;
@@ -13,9 +12,59 @@ export default function Polaris() {
   const allCirclesAvgPoint = [0, 0];
   const previousAllCirclesAvgPoint = [0, 0];
 
+  class Circle{
+    constructor(allCirclesAvgPoint, pullToTarget, pullToAvgPoint, recalculateThreshold, p5) {
+      this.currentPos = [randomInt(p5.width), randomInt(p5.height)];
+      this.lastTarget = this.currentPos;
+      this.nextTarget = [randomInt(p5.width), randomInt(p5.height)];
+      this.allCirclesAvgPoint = allCirclesAvgPoint;
+      this.pullToTarget = pullToTarget;
+      this.pullToAvgPoint = pullToAvgPoint;
+      this.recalculateThreshold = recalculateThreshold;
+    }
+
+    //global refers to the average point of all circles in the circle array
+    updateCurrentGlobalAvgPoint(circlesAvgPointGlobally, p5) {
+      this.allCirclesAvgPoint =  circlesAvgPointGlobally;
+      //If circle is close enough to target, get a new target
+      if(p5.dist(this.currentPos[0], this.currentPos[1], this.nextTarget[0], this.nextTarget[1]) < recalculateThreshold){
+        this.lastTarget = this.nextTarget;
+        this.nextTarget = (randomInt(p5.width), randomInt(p5.height));
+      }
+    }
+
+    display(p5) {    
+      p5.ellipse(this.currentPos[0], this.currentPos[1], 10, 10);
+    }
+
+    move(p5) {
+      const vectorMain = p5.PVector(this.nextTarget[0], this.nextTarget[1]);
+      vectorMain.setMag(this.pullToTarget);
+    
+      const vectorAvgPnt = p5.PVector(this.allCirclesAvgPoint[0], this.allCirclesAvgPoint[1]);
+      vectorAvgPnt.setMag(this.pullToAvgPnt);
+
+      const resultVector = p5.PVector.add(vectorMain, vectorAvgPnt);
+    
+      if(this.currentPos[0] > this.nextTarget[0]){
+        this.currentPos[0] -= resultVector.x;
+      }
+      if(this.currentPos[0] < this.nextTarget[0]){
+        this.currentPos[0] += resultVector.x;
+      }
+      if(this.currentPos[1] > this.nextTarget[1]){
+        this.currentPos[1] -= resultVector.y;
+      }
+      if(self.currentPos[1] < self.nextTarget[1]){
+        self.currentPos[1] += resultVector.y;
+      }
+    }
+  }
+
   const randomInt = (max, min = 0) => {
     return Math.ceil(Math.random() * (max - min) + min);
   };
+
 
   const setup = p5 => {
     p5.createCanvas(600, 600);
@@ -29,48 +78,6 @@ export default function Polaris() {
     }
   };
 
-  class Circle{
-    constructor(allCirclesAvgPoint, pullToTarget, pullToAvgPoint, recalculateThreshold, p5) {
-      this.currentPos = [randomInt(p5.width), randomInt(p5.height)];
-      this.lastTarget = this.currentPos;
-      this.nextTarget = [randomInt(p5.width), randomInt(p5.height)];
-      this.allCirclesAvgPoint = allCirclesAvgPoint;
-      this.pullToTarget = pullToTarget;
-      this.pullToAvgPoint = pullToAvgPoint;
-      this.recalculateThreshold = recalculateThreshold;
-    }
-
-    update(circlesAvgPointGlobally, p5) {
-      this.allCirclesAvgPoint =  circlesAvgPointGlobally;
-      //If circle is close enough to target, get a new target
-      if(p5.dist(this.currentPos[0], this.currentPos[1], this.nextTarget[0], this.nextTarget[1]) < recalculateThreshold){
-        this.lastTarget = this.nextTarget;
-        this.nextTarget = (randomInt(p5.width), randomInt(p5.height));
-      }
-    }
-
-    display(p5) {    
-      p5.ellipse(this.currentPos[0], this.currentPos[1], 10, 10);
-    }
-  }
-
-
-  //     def move(self):
-  //         vectorMain = PVector(self.nextTarget[0], self.nextTarget[1])
-  //         #print vectorMain
-  //         vectorMain.setMag(self.pullToTarget)
-  //         vectorAvgPnt = PVector(self.allCirAvgPnt[0], self.allCirAvgPnt[1])
-  //         vectorAvgPnt.setMag(self.pullToAvgPnt)
-  //         resultVector = PVector.add(vectorMain, vectorAvgPnt)
-  //         #print resultVector
-  //         if self.currentPos[0] > self.nextTarget[0]:
-  //             self.currentPos[0] -= resultVector.x
-  //         if self.currentPos[0] < self.nextTarget[0]:
-  //             self.currentPos[0] += resultVector.x
-  //         if self.currentPos[1] > self.nextTarget[1]:
-  //             self.currentPos[1] -= resultVector.y
-  //         if self.currentPos[1] < self.nextTarget[1]:
-  //             self.currentPos[1] += resultVector.y
   //         #self.currentPos += [resultVector.x, resultVector.y]
   //     def moveVec(self):
   //         newVel = vel
@@ -80,8 +87,10 @@ export default function Polaris() {
   
 
   const draw = p5 => {
-    p5.noLoop();
+    
   };
+
+  console.log('registered the draw loop');
   
   return (
     <Sketch setup={setup} draw={draw} />
