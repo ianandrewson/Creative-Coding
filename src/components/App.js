7ba886811323ec9fb92f760bd05c5ce2933a6a0f
Ian Andrewson <ianandrewson@gmail.com>
2020-03-13 17:06:33 -0700

    added sketch container to center canvas. Added parent to Polaris canvas. Need to add parent to all other sketches

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import {
   Route  
 } from 'react-router-dom';
 import Header from './header/Header.js';
+import styles from './App.css';
 import Dazed from './processing/dazed.js';
 import Shrink from './processing/Shrink.js';
 import Polaris from './processing/Polaris.js';
@@ -17,14 +18,16 @@ export default function App() {
     <>
       <Router>
         <Header />
-        <Switch>
-          <Route path='/polaris' component={Polaris} />
-          <Route path='/counter-rotate' component={CounterRotate} />
-          <Route path='/hello-sketch' component={HelloSketch} />
-          <Route path='/fractal-tree' component={FirstFractal} />
-          <Route path='/dazed' component={Dazed} />
-          <Route path='/shrink' component={Shrink} />
-        </Switch>
+        <div className={styles.sketchContainer}>
+          <Switch>
+            <Route path='/polaris' component={Polaris} />
+            <Route path='/counter-rotate' component={CounterRotate} />
+            <Route path='/hello-sketch' component={HelloSketch} />
+            <Route path='/fractal-tree' component={FirstFractal} />
+            <Route path='/dazed' component={Dazed} />
+            <Route path='/shrink' component={Shrink} />
+          </Switch>
+        </div>
       </Router>
     </>
   );
